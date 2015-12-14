f4098b9a80271d0a5b93ce9c4d505462c8399c2b
ungarj <joachim.ungar@gmail.com>
2015-12-14 17:23:45 +0100

    renaming source directory & adding first working setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='tilematrix',
-    version='0.1',
+    version='0.0.1',
     description='helps handling tile pyramids',
     author='Joachim Ungar',
     author_email='joachim.ungar@gmail.com',
     url='https://github.com/ungarj/tilematrix',
-    #   packages=[],
-    #py_moduels=['src.tilematrix', 'src.tilematrix_io']
-    package_dir = {'tilematrix': 'src'},
-    packages=["tilematrix"]
+    license='MIT',
+    packages=['tilematrix'],
+    install_requires=[
+        'rasterio',
+        'shapely',
+        'affine',
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ]
 )
