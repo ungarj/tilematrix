dd8b77b488c0ced917b2b5e39f44f49469dd6d87
ungarj <joachim.ungar@gmail.com>
2016-02-25 13:30:16 +0100

    fixing reprojection, adding geometry validation & fixing

--- tilematrix/io.py
+++ tilematrix/io.py
@@ -15,6 +15,7 @@ import numpy.ma as ma
 from copy import deepcopy
 from shapely.wkt import loads
 from shapely.ops import transform
+from shapely.validation import explain_validity
 import ogr
 from functools import partial
 import pyproj
@@ -337,11 +338,11 @@ def write_raster_window(
 #####################
 
 
-def file_bbox(input_file, out_crs, segmentize_maxlen=0.5):
+def file_bbox(input_file, out_crs, segmentize_maxlen=0.1):
     """
     Returns the bounding box of a raster or vector file in a given CRS.
     """
-
+    # Read raster data with rasterio, vector data with fiona.
     try:
         inp = rasterio.open(input_file)
         inp_crs = inp.crs
@@ -353,35 +354,42 @@ def file_bbox(input_file, out_crs, segmentize_maxlen=0.5):
         inp = fiona.open(input_file)
         inp_crs = inp.srs
         left, bottom, right, top = inp.bounds
-
-
+    # Create bounding box polygon.
     tl = [left, top]
     tr = [right, top]
     br = [right, bottom]
     bl = [left, bottom]
     bbox = Polygon([tl, tr, br, bl])
-
+    out_bbox = bbox
+    # If soucre and target CRSes differ, segmentize and reproject
     if inp_crs != out_crs:
         ogr_bbox = ogr.CreateGeometryFromWkb(bbox.wkb)
         ogr_bbox.Segmentize(segmentize_maxlen)
         segmentized = loads(ogr_bbox.ExportToWkt())
-        bbox = segmentized
-
-    project = partial(
-        pyproj.transform,
-        pyproj.Proj(inp_crs),
-        pyproj.Proj(out_crs)
-    )
-
-    projected_bbox = transform(project, bbox)
-
+        project = partial(
+            pyproj.transform,
+            pyproj.Proj(inp_crs),
+            pyproj.Proj(out_crs)
+        )
+        out_bbox = transform(project, segmentized)
+    else:
+        out_bbox = bbox
+    # Close input dataset.
     try:
         inp.close()
     except:
         pass
-
-
-    return projected_bbox
+    # Validate and, if necessary, try to fix output geometry.
+    try:
+        assert out_bbox.is_valid
+    except:
+        cleaned = out_bbox.buffer(0)
+        try:
+            assert cleaned.is_valid
+        except:
+            raise TypeError(cleaned.explain_validity)
+        out_bbox = cleaned
+    return out_bbox
 
 
 def _read_band_to_tile(
