25154803a9be1f58e75bb2df94a4d0b6018ddc57
ungarj <joachim.ungar@gmail.com>
2016-08-23 18:02:01 +0200

    prepare 0.2 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tilematrix',
-    version='0.1',
+    version='0.2',
     description='helps handling tile pyramids',
     author='Joachim Ungar',
     author_email='joachim.ungar@gmail.com',
