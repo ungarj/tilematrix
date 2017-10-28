012a896677bbe737ee6fc6e8be315898f843cdb0
Joachim Ungar <joachim.ungar@eox.at>
2017-10-28 11:03:57 +0200

    removed other python versions from travis, added gdal-bin

--- setup.py
+++ setup.py
@@ -24,10 +24,7 @@ setup(
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6'
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
