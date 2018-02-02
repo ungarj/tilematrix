089276e38c11c7d355c63e212ec1a5764925cbbe
ungarj <joachim.ungar@gmail.com>
2018-02-02 12:10:59 +0100

    enable GridDefinition init with other GridDefinition object

--- tilematrix/_funcs.py
+++ tilematrix/_funcs.py
@@ -68,39 +68,43 @@ class GridDefinition(object):
         if isinstance(grid_definition, dict):
             self.init_definition = dict(**grid_definition)
             self.type = "custom"
-            if "shape" not in grid_definition:
+            if "shape" not in self.init_definition:
                 raise AttributeError("grid shape not provided")
-            self.shape = Shape(*grid_definition["shape"])
-            self.bounds = Bounds(*grid_definition["bounds"])
+            self.shape = Shape(*self.init_definition["shape"])
+            self.bounds = Bounds(*self.init_definition["bounds"])
             # verify that shape aspect ratio fits bounds apsect ratio
             _verify_shape_bounds(shape=self.shape, bounds=self.bounds)
             self.left, self.bottom, self.right, self.top = self.bounds
-            self.is_global = grid_definition.get("is_global", False)
-            if all([i in grid_definition for i in ["proj", "epsg"]]):
+            self.is_global = self.init_definition.get("is_global", False)
+            if all([i in self.init_definition for i in ["proj", "epsg"]]):
                 raise ValueError("either 'epsg' or 'proj' are allowed.")
-            if "epsg" in grid_definition:
-                self.crs = CRS().from_epsg(grid_definition["epsg"])
-                self.srid = grid_definition["epsg"]
-            elif "proj" in grid_definition:
-                self.crs = CRS().from_string(grid_definition["proj"])
+            if "epsg" in self.init_definition:
+                self.crs = CRS().from_epsg(self.init_definition["epsg"])
+                self.srid = self.init_definition["epsg"]
+            elif "proj" in self.init_definition:
+                self.crs = CRS().from_string(self.init_definition["proj"])
                 self.srid = None
             else:
                 raise AttributeError("either 'epsg' or 'proj' is required")
         elif isinstance(grid_definition, six.string_types):
-            if grid_definition not in PYRAMID_PARAMS:
+            self.init_definition = grid_definition
+            if self.init_definition not in PYRAMID_PARAMS:
                 raise ValueError(
                     "WMTS tileset '%s' not found. Use one of %s" % (
-                        grid_definition, PYRAMID_PARAMS.keys()))
-            self.init_definition = grid_definition
-            self.type = grid_definition
+                        self.init_definition, PYRAMID_PARAMS.keys()))
+            self.type = self.init_definition
             self.shape = Shape(
-                *PYRAMID_PARAMS[grid_definition]["shape"])
+                *PYRAMID_PARAMS[self.init_definition]["shape"])
             self.bounds = Bounds(
-                *PYRAMID_PARAMS[grid_definition]["bounds"])
+                *PYRAMID_PARAMS[self.init_definition]["bounds"])
             self.left, self.bottom, self.right, self.top = self.bounds
-            self.is_global = PYRAMID_PARAMS[grid_definition]["is_global"]
-            self.srid = PYRAMID_PARAMS[grid_definition]["epsg"]
+            self.is_global = PYRAMID_PARAMS[self.init_definition]["is_global"]
+            self.srid = PYRAMID_PARAMS[self.init_definition]["epsg"]
             self.crs = CRS().from_epsg(self.srid)
+        elif isinstance(grid_definition, GridDefinition):
+            self.__init__(
+                grid_definition.init_definition, tile_size=tile_size,
+                metatiling=metatiling)
         else:
             raise TypeError(
                 "invalid grid definition type: %s" % type(grid_definition))
